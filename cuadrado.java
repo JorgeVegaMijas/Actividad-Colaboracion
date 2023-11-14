dcc3653b1980a9688a08e1053c48a7a585964f9a
Tolvastor <122613740+Tolvastor@users.noreply.github.com>
2023-11-14 09:13:39 +0100

    Add files via upload

---
+++ cuadrado.java
@@ -0,0 +1,21 @@
+package jav;
+
+import java.util.Scanner;
+
+public class cuadrado {
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("dime la base del rectangulo");
+        int base = sc.nextInt();
+        
+        System.out.println("dime la altura del rectangulo");
+        int altura = sc.nextInt();
+    
+        int resultado = base * altura;
+
+        System.out.println("el area del rectangulo es: " + resultado);
+    }
+}
---
+++ triangulo.java
@@ -0,0 +1,19 @@
+package jav;
+
+import java.util.Scanner;
+
+public class triangulo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("dime la base del triangulo");
+        int base = sc.nextInt();
+        
+        System.out.println("dime la altura del triangulo");
+        int altura = sc.nextInt();
+    
+        int resultado = (base * altura) /2;
+
+        System.out.println("el area del triangulo es: " + resultado);
+    }
+}
